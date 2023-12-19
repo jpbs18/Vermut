f3e00c9f373707c503b157daf452926724987a3b
João Santos <joao.santos@mindswap.academy>
2023-12-19 21:20:17 +0000

    Refactored useResponsive hook

--- hooks/useResponsive.jsx
+++ hooks/useResponsive.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 
-const getwindowWidth = () => window.innerWidth
+const getwindowWidth = () => window.innerWidth;
 
 export const useResponsive = () => {
-  const [windowWidth, setwindowWidth] = useState(getwindowWidth())
+  const [windowWidth, setwindowWidth] = useState(getwindowWidth());
 
-  const handleResize = () => setwindowWidth(getwindowWidth())
+  const handleResize = () => setwindowWidth(getwindowWidth());
 
   useEffect(() => {
-    window.addEventListener('resize', () => setwindowWidth(getwindowWidth()))
-    return () => window.removeEventListener('resize', handleResize)
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return windowWidth <= 450
-}
+  return windowWidth <= 450;
+};
